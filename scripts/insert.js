dd77f9b0c4eae8a5fbc975c58509103d114b3ef6
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-05-04 19:39:36 +0200

    Btn Toggle

--- scripts/insert.js
+++ scripts/insert.js
@@ -19,6 +19,22 @@ function modalInteraction() {
   }
 }
 
+// Add toggle effect to Modal
+function modalToggle() {
+  var modalTom = document.getElementById("TomModal");
+  console.log (modalTom);
+
+  var show = document.getElementById("show");
+  console.log (show);
+
+  show.addEventListener('click', () => {
+    modalTom.classList.toggle('is-invisible');
+  });
+}
+
+
+
+
 
 function insertModal() {
   var body = document.querySelector('body');
@@ -29,26 +45,24 @@ function insertModal() {
     position: fixed;
     right: 0;
     top: 0;
-    height: 550px;
+    height: 100%;
     width: 30%;
+    margin: 40px 0px 0px 0px;
   }
 
   .modal-content {
     background-color: #fefefe;
-    margin: 15% auto;
     border: 1px solid #888;
     width: 100%;
     height: 100%;
-    border-radius: 20px;
     position: relative;
   }
 
-  iframe {
+  .modal-content > iframe {
     position: absolute;
     top: 0px;
     left: 0px;
     z-index: 1;
-    border-radius: 20px;
     overflow: scroll;
   }
 
@@ -68,18 +82,40 @@ function insertModal() {
     cursor: pointer;
   }
 
+  #TomModal.is-invisible {
+    display: none;
+  }
+
+  #show {
+
+    color: #aaa;
+    position: fixed;
+    top: 40px;
+    right: 150px;
+    font-size: 24px;
+    font-weight: bold;
+    z-index: 1000;
+    background-color: #283E4A;
+  }
+
+  #show:hover, #show:focus {
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
   </style>`
 
   // inserting modal in HTML
     body.insertAdjacentHTML('afterbegin',
       `${modalStyle}
+      <span id="show">Show</span>
       <div id="TomModal" class="modal-container">
       <div class="modal-content">
       <span id="closeTomModal">&times;</span>
-      <iframe src="https://readymag.com/u38532320/1883919/" width=100% height=100%></iframe>
+      <iframe src="https://readymag.com/u38532320/1887376/" width=100% height=80%></iframe>
       </div>
       </div>`);
-    modalInteraction();}
+    modalInteraction();
+    modalToggle();}
   insertModal();
-
-
